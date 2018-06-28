0cae8f959e4c1c203742849d75d9a8434be6bb52
MLoretto <loreto.henriquez.m@gmail.com>
2018-06-27 20:42:03 -0400

    Eventos y funciones de botones menú

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 let infoBip;
+const btnVerSaldo = document.getElementById('btnVerSaldo');
+const btnCalcTarifa = document.getElementById('btnCalcTarifa');
+const btnPerfil = document.getElementById('btnPerfil');
+const btnPregFrecuentes = document.getElementById('btnPregFrecuentes');
 
 window.getBipData(bipNumber) {
     Promise.all([
@@ -17,5 +21,38 @@ window.getBipData(bipNumber) {
   infoBip["Saldo  tarjeta"];
   infoBip["Fecha saldo"];
 */
+btnVerSaldo.addEventListener('click', () => {
+    verSaldo();
+  });
 
+btnCalcTarifa.addEventListener('click', () => {
+    calcularTarifa();
+});
 
+btnPerfil.addEventListener('click', () => {
+    verPerfil();
+});
+
+btnPregFrecuentes.addEventListener('click', () => {
+    pregFrecuentes();
+});
+
+function verSaldo(){
+    hideContent();
+}
+function calcularTarifa(){
+    hideContent();
+}
+function verPerfil(){
+    hideContent();
+}
+function pregFrecuentes(){
+    hideContent();
+}
+
+function hideContent() {// Oculta contenidos
+    const bodyContentChild = document.getElementById('bodyContent').children;
+    for (let i = 0;i < bodyContentChild.length;i++) {
+      bodyContentChild[i].style.display = 'none';// Los oculta uno a uno.
+    }   
+  };
